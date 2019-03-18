ca7be47f0f05afe6d30b0dfc5cc7222469b1868f
Caleb Fletcher <caleb@fletcher.cf>
2019-03-18 15:29:45 +1100

    Fix docker-compose

--- dist/index.php
+++ dist/index.php
@@ -1,12 +1,5 @@
 <?php
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
+require __DIR__ . '/vendor/autoload.php';
 
-// create a log channel
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('your.log', Logger::WARNING));
-
-// add records to the log
-$log->warning('Foo');
-$log->error('Bar');
+echo "<h1>Docker-PHP-Nginx-Boilerplate</h1>"
