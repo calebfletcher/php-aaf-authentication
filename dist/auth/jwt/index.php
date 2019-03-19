32edb62957982b7e7c2f1bfb97d74c7c919710ce
Caleb Fletcher <caleb@fletcher.cf>
2019-03-19 14:47:50 +1100

    Make DB use persistent storage

--- dist/auth/jwt/index.php
+++ dist/auth/jwt/index.php
@@ -3,24 +3,38 @@
 require_once __DIR__ . "/../../includes/common.php";
 use \Firebase\JWT\JWT;
 
-try {
-    $decoded = JWT::decode($_POST["assertion"], $config["secret_key"], array("HS256"));
-    if (isset($decoded->iss) && $decoded->iss === $config["auth_issuer"]) {
-        echo "issuer good";
-    }
-    if (isset($decoded->aud) && $decoded->aud === $config["auth_audience"]) {
-        echo "audience good";
-    }
-    $decoded_array = (array) $decoded;
-    print_r($decoded_array);
-} catch (Firebase\JWT\SignatureInvalidException $e) {
-    echo $e->getMessage();
-} catch (Firebase\JWT\ExpiredException $e) {
-    echo $e->getMessage();
-} catch (Firebase\JWT\BeforeValidException $e) {
-    echo $e->getMessage();
-} catch (UnexpectedValueException $e) {
-    echo "Malformed Token: ".$e->getMessage();
-} catch (Exception $e) {
-    echo "Token Decode Error: ".$e->getMessage();
+$conn = new PDO($config["db_uri"], $config["db_username"], $config["db_password"]);
+
+/*$decoded = JWT::decode($_POST["assertion"], $config["secret_key"], array("HS256"));
+
+if (!isset($decoded->iss) || $decoded->iss !== $config["auth_issuer"]) {
+    throw new UnexpectedValueException("Invalid Issuer");
 }
+if (!isset($decoded->aud) && $decoded->aud !== $config["auth_audience"]) {
+    throw new UnexpectedValueException("Invalid audience");
+}
+
+$attributes = $decoded->{"https://aaf.edu.au/attributes"};
+print_r($attributes);
+*/
+
+$stmt = $conn->prepare("SELECT email,edupersontargetedid,displayname,organisation FROM web_user WHERE email LIKE ?");
+$status = $stmt->execute(["%"]);
+if (!status) {
+    throw new PDOException($conn->errorInfo()[2]);
+}
+$res = $stmt->fetchAll();
+echo "<br /><br />";
+foreach ($res as $row) {
+    print_r($row);
+}
+
+/*if ($stmt->execute()) {
+    echo $attributes->{"mail"};
+    while ($row = $stmt->fetch()) {
+        print_r($row);
+    }
+}
+else {
+    echo "error";
+}*/
--- dist/includes/common.php
+++ dist/includes/common.php
@@ -1,4 +1,9 @@
 <?php
 
+function exception_handler($exception) {
+    echo get_class($exception) . ": " . $exception->getMessage();
+}
+set_exception_handler('exception_handler');
+
 require_once __DIR__ . "/config.php";
 require_once __DIR__ . "/../vendor/autoload.php";
